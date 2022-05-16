180b9b43e5de3ed15d232b959b76bf77a176c005
Dorofeiev <105375365+Dorofeiev@users.noreply.github.com>
2022-05-16 21:23:22 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-    id = models.BigAutoField(primary_key=True)
 
     def publish(self):
         self.published_date = timezone.now()
